e4dad64dcdd40ac6e843b153b87c774d24921678
Hristo Hristov <hristo.utd@gmail.com>
2025-05-23 16:58:07 +0300

    (feat/02)-first lesson practice

---
+++ pw-practice-app/tests/uiComponents.spec.ts
@@ -0,0 +1,28 @@
+import { expect, test } from '@playwright/test';
+import { delay } from 'rxjs-compat/operator/delay';
+
+test.beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:4200/');
+})
+
+test.describe('Form Layouts page', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.getByText('Forms').click();
+        await page.getByText('Form Layouts').click();
+    })
+
+    test('Input fields', async ({ page }) => {
+        const usingTheGridEmailInput = page.locator('nb-card', { hasText: 'Using the Grid' }).getByRole('textbox', { name: 'Email' });
+
+        await usingTheGridEmailInput.fill('test@test.com');
+        await usingTheGridEmailInput.clear();
+        await usingTheGridEmailInput.pressSequentially('test2@test.com', {delay: 200});
+
+        //generic assertion
+        const inputValue = await usingTheGridEmailInput.inputValue();
+        expect(inputValue).toEqual('test2@test.com');
+
+        //locatior assertion
+        await expect(usingTheGridEmailInput).toHaveValue('test2@test.com');
+    })
+});
