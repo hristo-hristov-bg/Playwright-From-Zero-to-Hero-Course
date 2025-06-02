e6e7a4adb75414d3296ca36254d7977c5182b0f2
Hristo Hristov <hristo.utd@gmail.com>
2025-06-02 17:51:07 +0300

    (feat/02):lesson 3 & 4 practice

--- pw-practice-app/tests/uiComponents.spec.ts
+++ pw-practice-app/tests/uiComponents.spec.ts
@@ -16,7 +16,7 @@ test.describe('Form Layouts page', () => {
 
         await usingTheGridEmailInput.fill('test@test.com');
         await usingTheGridEmailInput.clear();
-        await usingTheGridEmailInput.pressSequentially('test2@test.com', {delay: 200});
+        await usingTheGridEmailInput.pressSequentially('test2@test.com', { delay: 200 });
 
         //generic assertion
         const inputValue = await usingTheGridEmailInput.inputValue();
@@ -40,4 +40,57 @@ test.describe('Form Layouts page', () => {
         expect(await usingTheGridForm.getByRole('radio', { name: 'Option 1' }).isChecked()).toBeFalsy();
         expect(await usingTheGridForm.getByRole('radio', { name: 'Option 2' }).isChecked()).toBeTruthy();
     })
+});
+
+
+test('checkboxes', async ({ page }) => {
+    await page.getByText('Modal & Overlays').click();
+    await page.getByText('Toastr').click();
+
+    await page.getByRole('checkbox', { name: "Hide on click" }).uncheck({ force: true });
+    await page.getByRole('checkbox', { name: "Prevent arising of duplicate toast" }).check({ force: true });
+
+    const allBoxes = page.getByRole('checkbox');
+    // all checkboxes are checked
+    // for (const box of await allBoxes.all()) {
+    //     await box.check({ force: true });
+    //     expect(await box.isChecked()).toBeTruthy();
+    // }
+
+    for (const box of await allBoxes.all()) {
+        await box.uncheck({ force: true });
+        expect(await box.isChecked()).toBeFalsy();
+    }
+});
+
+test('lists and dropdowns', async ({ page }) => {
+    const dropdownMenu = page.locator('ngx-header nb-select');
+    await dropdownMenu.click();
+
+    page.getByRole('list'); // when the list has a UL tag
+    page.getByRole('listitem'); // when the list has a LI tag
+
+    // const optionList = page.getByRole('list').locator('nb-option');
+    const optionList = page.locator('nb-option-list nb-option');
+    await expect(optionList).toHaveText(["Light", "Dark", "Cosmic", "Corporate"]);
+    await optionList.filter({ hasText: 'Cosmic' }).click();
+
+    const header = page.locator('nb-layout-header');
+    await expect(header).toHaveCSS('background-color', 'rgb(50, 50, 89)'); // Cosmic theme color
+
+    const colors = {
+        Light: 'rgb(255, 255, 255)',
+        Dark: 'rgb(34, 43, 69)',
+        Cosmic: 'rgb(50, 50, 89)',
+        Corporate: 'rgb(255, 255, 255)'
+    }
+
+    await dropdownMenu.click();
+    for (const color in colors) {
+        await optionList.filter({ hasText: color }).click();
+        await expect(header).toHaveCSS('background-color', colors[color]);
+        if (color !== 'Corporate') {
+            await dropdownMenu.click();
+        }
+    }
 });
