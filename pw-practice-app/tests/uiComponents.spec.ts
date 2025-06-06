66b7e0a3aa0bfdb2389edf2bdb17fe8edb0411f5
Hristo Hristov <hristo.utd@gmail.com>
2025-06-06 17:04:10 +0300

    (feat/02):lessons 7-10 practice

--- pw-practice-app/tests/uiComponents.spec.ts
+++ pw-practice-app/tests/uiComponents.spec.ts
@@ -43,7 +43,7 @@ test.describe('Form Layouts page', () => {
 });
 
 
-test('checkboxes', async ({ page }) => {
+test('Checkboxes', async ({ page }) => {
     await page.getByText('Modal & Overlays').click();
     await page.getByText('Toastr').click();
 
@@ -63,7 +63,7 @@ test('checkboxes', async ({ page }) => {
     }
 });
 
-test('lists and dropdowns', async ({ page }) => {
+test('Lists and dropdowns', async ({ page }) => {
     const dropdownMenu = page.locator('ngx-header nb-select');
     await dropdownMenu.click();
 
@@ -95,7 +95,7 @@ test('lists and dropdowns', async ({ page }) => {
     }
 });
 
-test('tooltips', async ({ page }) => {
+test('Tooltips', async ({ page }) => {
     await page.getByText('Modal & Overlays').click();
     await page.getByText('Tooltip').click();
 
@@ -107,7 +107,7 @@ test('tooltips', async ({ page }) => {
     expect(tooltip).toEqual('This is a tooltip');
 });
 
-test('dialog box', async ({ page }) => {
+test('Dialog box', async ({ page }) => {
     await page.getByText('Tables & Data').click();
     await page.getByText('Smart Table').click();
 
@@ -119,3 +119,96 @@ test('dialog box', async ({ page }) => {
     await page.getByRole('table').locator('tr', { hasText: "mdo@gmail.com" }).locator('.nb-trash').click();
     await expect(page.locator('table tr').first()).not.toHaveText('mdo@gmail.com');
 })
+
+test('Web tables', async ({ page }) => {
+    await page.getByText('Tables & Data').click();
+    await page.getByText('Smart Table').click();
+
+    // 1 - get the row by any text in this row
+    const targetRow = page.getByRole('row', { name: 'twitter@outlook.com'});
+    await targetRow.locator('.nb-edit').click();
+    await page.locator('input-editor').getByPlaceholder('Age').clear();
+    await page.locator('input-editor').getByPlaceholder('Age').fill('35');
+    await page.locator('.nb-checkmark').click();
+
+    // 2 - get the row based on the value in the specific column
+    await page.locator('.ng2-smart-pagination-nav').getByText('2').click();
+    const targetRowById = page.getByRole('row', {name: '11'}).filter({ has: page.locator('td').nth(1).getByText('11') });
+    await targetRowById.locator('.nb-edit').click();
+    await page.locator('input-editor').getByPlaceholder('E-mail').clear();
+    await page.locator('input-editor').getByPlaceholder('E-mail').fill('test@test.com');
+    await page.locator('.nb-checkmark').click();
+    await expect(targetRowById.locator('td').nth(5)).toHaveText('test@test.com');
+
+    // 3 - test filter of the table
+    const ages = ["20", "30", "40", "200"];
+
+    for(let age of ages) {
+        await page.locator('input-filter').getByPlaceholder('Age').clear();
+        await page.locator('input-filter').getByPlaceholder('Age').fill(age);
+        await page.waitForTimeout(500); // wait for the table to filter
+        const ageRows = page.locator('tbody tr');
+
+        for(let row of await ageRows.all()){
+            const cellValue = await row.locator('td').last().textContent();
+
+            if(age === "200") {
+                expect(await page.getByRole('table').textContent()).toContain('No data found');
+            } else {
+                expect(cellValue).toEqual(age);
+            }
+        }
+    }
+});
+
+test('Date picker', async ({ page }) => {
+    await page.getByText('Forms').click();
+    await page.getByText('Datepicker').click();
+
+    const calendarInputField = page.getByPlaceholder('Form Picker');
+    await calendarInputField.click();
+
+    let date = new Date();
+    date.setDate(date.getDate() + 30);
+    const expectedDate = date.getDate().toString();
+    const expectedMonthShort = date.toLocaleString('En-US', { month: 'short' });
+    const expectedMonthLong = date.toLocaleString('En-US', { month: 'long' });
+    const expectedYear = date.getFullYear();
+    const dateToAssert = `${expectedMonthShort} ${expectedDate}, ${expectedYear}`;
+
+    let calendarMonthAndYear = await page.locator('nb-calendar-view-mode').textContent();
+    const expectedMonthAndYear = ` ${expectedMonthLong} ${expectedYear} `;
+    while (!calendarMonthAndYear.includes(expectedMonthAndYear)) {
+        await page.locator('nb-calendar-pageable-navigation [data-name="chevron-right"]').click();
+        calendarMonthAndYear = await page.locator('nb-calendar-view-mode').textContent();
+    }
+
+    await page.locator('[class="day-cell ng-star-inserted"]').getByText(expectedDate, {exact: true}).click();
+    await expect(calendarInputField).toHaveValue(dateToAssert);
+});
+
+test('Sliders', async ({ page }) => {
+    // 1st option -  update attribute
+    // const tempGauge = page.locator('[tabtitle="Temperature"] ngx-temperature-dragger circle');
+    // await tempGauge.evaluate( node => {
+    //     node.setAttribute('cx', '232.630');
+    //     node.setAttribute('cy', '232.630');
+    // })
+
+    // await tempGauge.click();
+
+    // 2-nd option - mouse movement
+    const tempBox = page.locator('[tabtitle="Temperature"] ngx-temperature-dragger');
+    await tempBox.scrollIntoViewIfNeeded();
+
+    const box = await tempBox.boundingBox();
+    const x = box.x + box.width / 2;
+    const y = box.y + box.height / 2;
+    await page.mouse.move(x, y);
+    await page.mouse.down();
+    await page.mouse.move(x + 100, y);
+    await page.mouse.move(x + 100, y + 100);
+    await page.mouse.up();
+
+    await expect(tempBox).toContainText('30');
+});
